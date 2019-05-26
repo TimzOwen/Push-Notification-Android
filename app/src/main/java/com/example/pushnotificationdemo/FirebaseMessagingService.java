45c32c0bd0e9a87526c151e3279d82a293dffbee
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-05-26 13:11:02 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/pushnotificationdemo/FirebaseMessagingService.java
@@ -0,0 +1,20 @@
+package com.example.pushnotificationdemo;
+
+
+import android.app.PendingIntent;
+import android.content.Intent;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+class FirebaseMessagingServicer extends FirebaseMessagingService
+{
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+    }
+}
