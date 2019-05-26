ffffa72bc524fad263000283088a0d6ce6217702
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-05-26 06:22:05 -0400

    Update FirebaseMessagingService.java
    get the notification an update on the user remote without data

--- app/src/main/java/com/example/pushnotificationdemo/FirebaseMessagingService.java
+++ app/src/main/java/com/example/pushnotificationdemo/FirebaseMessagingService.java
@@ -16,5 +16,21 @@ class FirebaseMessagingServicer extends FirebaseMessagingService
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        
+        
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setContentTitle("ALC Android Track");
+        notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //show notification and build to show
+        notificationManager.notify(0,notificationBuilder.build());
+
+
+        
+        
     }
 }
